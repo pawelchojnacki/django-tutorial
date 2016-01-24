b0b71089dc070071853e361ee6f17f4e719416a0
Szymon Chaber <Zragon>
2016-01-24 22:57:17 +0100

    Made some new views, got more familiar with RegEx, introduced templates to myself

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.indeksik, name='index'),
-    url(r'czylizetak', views.aha, name='ixdex')
+    #np.: /polls/
+    url(r'^$', views.index, name='index'),
+    #np.: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    #np.: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    #np.: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
